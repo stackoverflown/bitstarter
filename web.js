a8ce028b9f451055fc18121076e596c37cce690b
Glux <granvillelintao2@gmail.com>
2013-07-18 09:18:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var out = buf.toString( fs.readFileSync('index.html');
+  
+  response.send(out);
+
 });
 
 var port = process.env.PORT || 5000;
